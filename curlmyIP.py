89f5ba94e169d82b7a84b819f3bff6b744506c77
dchin0 <48170946+dchin0@users.noreply.github.com>
2021-07-09 22:26:33 -0700

    Update curlmyIP.py

--- curlmyIP.py
+++ curlmyIP.py
@@ -1,5 +1,9 @@
 import urllib.request
 
 response = urllib.request.urlopen("http://checkip.amazonaws.com")
-print(response.readline())
-print(response.close())
+result = response.readline()
+
+print(result)
+print(result.find(b'76.94.30'))
+
+response.close()

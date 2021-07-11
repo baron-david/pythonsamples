fc872d08119c32ea79b7294d4f973c43bc0006c9
dchin0 <48170946+dchin0@users.noreply.github.com>
2021-07-10 22:53:43 -0700

    Update curlmyIP.py

--- curlmyIP.py
+++ curlmyIP.py
@@ -1,6 +1,7 @@
 import urllib.request
 
-response = urllib.request.urlopen("http://checkip.amazonaws.com")
+#response = urllib.request.urlopen("http://checkip.amazonaws.com")
+response = urllib.request.urlopen("https://domains.google.com/checkip")
 result = response.readline()
 
 print(result)
